f558eafddd9dfbee23f520fc0f9b43541cce95ed
Ankur Soni <ankur.soni@outlook.com>
2020-10-12 05:22:08 +1100

    Refactor

---
+++ fortosi-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $1 - git clone directory (optional - default = kubernetes-extension-fortosi)
+# $2 - branch name (optional - default = master)
+
+GIT_REPO="https://github.com/ankursoni/kubernetes-extension-fortosi.git"
+GIT_CLONE_PATH=$(if [ -z "$1" ]; then echo ""; else echo "$1"; fi)
+GIT_BRANCH=$(if [ -z "$2" ]; then echo "master"; else echo "$2"; fi)
+
+echo -e "\nCloning git - ${GIT_REPO} from branch - ${GIT_BRANCH} into directory - $GIT_CLONE_PATH"
+git clone $GIT_REPO --branch $GIT_BRANCH $GIT_CLONE_PATH
+
+echo -e "\nInstalling kubernetes extension from ${GIT_REPO}"
+sudo cp $GIT_CLONE_PATH/scripts/auto-setup.sh /usr/local/bin/kubectl-fortosi
+sudo chmod +x /usr/local/bin/kubectl-fortosi
